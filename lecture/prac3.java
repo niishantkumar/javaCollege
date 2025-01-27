377fc02424e71e16ef2e9e318e8e243ebae64214
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-27 20:53:33 +0530

    prac3.java modified

--- lecture/prac3.java
+++ lecture/prac3.java
@@ -2,5 +2,30 @@ package javaCollege.lecture;
 //method overloading
 
 public class prac3 {
+    public static void main(String[] args) {
+        A ob = new A();
+        ob.show();
+        ob.show(30);
+        ob.show(40,50);
+    }
     
+}
+
+class A{
+    int x =10, y = 20;
+
+    void show(){
+        System.out.println("x = "+x+" y = "+y);
+    }
+
+    void show(int p){
+        x = y = p;
+        System.out.println("x = "+x+" y = "+y);
+    }
+
+    void show(int p,int q){
+        x =  p;
+        y = q;
+        System.out.println("x = "+x+" y = "+y);
+    }
 }
