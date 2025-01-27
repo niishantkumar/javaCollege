b935cb2acc641703f400919a533f74c4dab37a16
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-27 17:24:20 +0530

    prac1.java modified

--- lecture/prac1.java
+++ lecture/prac1.java
@@ -1,5 +1,46 @@
 package javaCollege.lecture;
 
+//constructors
+
 public class prac1 {
-    
+
+    public static void main(String[] args) {
+        A ob = new A();
+        A ob1 = new A(100);
+        A ob2 = new A(200, 300);
+        A ob3 = new A(ob2);
+
+        ob.show();
+        ob1.show();
+        ob2.show();
+        ob3.show();
+    }
 }
+
+class A {
+    int x, y;
+
+    A() {          //non-parameterized
+        x = 10;
+        y = 20;
+    }
+
+    A(int p) {          //parameterized
+        x = y = p;
+    }
+
+    A(int p, int q) {    //parameterized
+        x = p;
+        y = q;
+    }
+
+    A(A ob) {      //copy cons
+        x = ob.x;
+        y = ob.y;
+    }
+
+    void show() {
+        System.out.println("x = " + x + " y = " + y);
+    }
+}
+
