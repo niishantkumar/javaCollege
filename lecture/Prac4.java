47dd83856a81dca7f450719696701b9c0fefb6fb
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-02-12 20:10:11 +0530

    Prac4.java modified

--- lecture/Prac4.java
+++ lecture/Prac4.java
@@ -5,5 +5,18 @@ package javaCollege.lecture;
 import java.io.*;
 
 public class Prac4 {
-    
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        // input of numbers
+        System.out.print("Enter 1st number : ");
+        int num1 = Integer.parseInt(bf.readLine());
+        System.out.print("Enter 2nd number : ");
+        int num2 = Integer.parseInt(bf.readLine());
+
+        //print sum
+        System.out.println("Sum is " + (num1 + num2));
+    }
+
 }
