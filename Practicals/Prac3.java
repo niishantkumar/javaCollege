bea947593949f45b49bb85245ba0c3a57198e193
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-04-16 22:28:55 +0530

    Prac3.java added

---
+++ Practicals/Prac3.java
@@ -0,0 +1,19 @@
+package javaCollege.Practicals;
+
+public class Prac3 {
+    public static void main(String[] args) {
+        System.out.println();
+        String s1 = "JAVA is A LANG";
+        String s2 = s1.toLowerCase();
+        System.out.println(s2);
+
+        String s3 = s1.toUpperCase();
+        System.out.println(s3);
+
+        String s4 = "   Hey   Boy   ";
+        String s5 = s4.trim();
+        System.out.println(s5);
+        System.out.println();
+    }
+
+}
