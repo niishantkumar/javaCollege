7499af17ba221d007bc252e3699a61bb4dce74ce
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-04-16 20:26:53 +0530

    Prac2.java added

---
+++ Practicals/Prac2.java
@@ -0,0 +1,9 @@
+public class Prac2 {
+    public static void main(String args[]){
+        char c[] = {'a', 'b','c','d','e','f','g'};
+        String s = new String(c);
+        System.out.println();
+        System.out.println(s);
+        System.out.println();
+    }
+}
